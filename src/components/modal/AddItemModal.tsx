8e8fdfb2ca4ea20e77be8f99fc3fd49776160d87
ramkrishna0710 <113228237+ramkrishna0710@users.noreply.github.com>
2025-04-09 09:23:30 +0530

    Adding AddItemModal

--- src/components/modal/AddItemModal.tsx
+++ src/components/modal/AddItemModal.tsx
@@ -1,15 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { FC, useEffect, useState } from 'react'
 import { useAppDispatch } from '@states/reduxHook';
 import { useStyles } from 'react-native-unistyles';
 import { modelStyles } from '@unistyles/modelStyles';
 import { addCustomizableItem } from '@states/reducers/cartSlice';
+import CustomText from '@components/global/CustomText';
+import Icon from '@components/global/Icon';
+import { Colors } from '@unistyles/Constants';
+import DottedLine from '@components/ui/DottedLine';
+import ScalePress from '@components/ui/ScalePress';
+import { RFValue } from 'react-native-responsive-fontsize';
+import AnimatedNumber from 'react-native-animated-numbers';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+
+
+function transformSelectedOptions(
+    selectedOption: any,
+    customizaionOptions: any,
+) {
+    return Object.entries(selectedOption).map(([type, index]) => {
+        const customization = customizaionOptions?.find(
+            (option: any) => option.type === type
+        );
+        if (!customization || !customization?.options[index as number]) {
+            throw new Error(`Invalid customization type or index for ${type}`)
+        }
+        return {
+            type,
+            selectedOption: customization?.options[index as number]
+        }
+    })
+}
 
 const AddItemModal: FC<{ item: any; resturant: any; onClose: () => void }> = ({
     item,
     resturant,
     onClose
 }) => {
+
+    // console.log(item);
+
 
     const dispatch = useAppDispatch()
     const { styles } = useStyles(modelStyles)
@@ -86,7 +117,7 @@ const AddItemModal: FC<{ item: any; resturant: any; onClose: () => void }> = ({
     }
 
     const removeCartHandler = () => {
-        if(data?.quantity > 1) {
+        if (data?.quantity > 1) {
             setData(prevData => ({
                 ...prevData,
                 quantity: prevData?.quantity - 1,
@@ -95,28 +126,145 @@ const AddItemModal: FC<{ item: any; resturant: any; onClose: () => void }> = ({
         }
     }
 
-    // const addItemIntoCart = async () => {
-    //     const customizaionOptions = transformSelectedOptions(
-    //         data?.selectedOption,
-    //         item?.customizaionOptions,
-    //     ).sort((a,b) =>  a.type.localeCompare(b.type))
+    const addItemIntoCart = async () => {
+        const customizaionOptions = transformSelectedOptions(
+            data?.selectedOption,
+            item?.customizaionOptions,
+        ).sort((a, b) => a.type.localeCompare(b.type))
 
-    //     const customizedData = {
-    //         resturant: resturant,
-    //         item: item,
-    //         customization: {
-    //             quantity: data?.quantity,
-    //             price: data?.price,
-    //             customizaionOptions: customizaionOptions,
-    //         }
-    //     }
-    //     dispatch(addCustomizableItem(customizedData))
-    //     onClose()
-    // }
+        const customizedData = {
+            resturant: resturant,
+            item: item,
+            customization: {
+                quantity: data?.quantity,
+                price: data?.price,
+                customizaionOptions: customizaionOptions,
+            }
+        }
+        dispatch(addCustomizableItem(customizedData))
+        onClose()
+    }
 
     return (
         <View>
-            <Text>AddItemModal</Text>
+            <View style={styles.headerContainer}>
+                <View style={styles.flexRowGap}>
+                    <Image source={{ uri: item?.image }} style={styles.headerImage} />
+                    <CustomText fontFamily='Okra-Medium' fontSize={12}>
+                        {item?.name}
+                    </CustomText>
+                </View>
+
+                <View style={styles.flexRowGap}>
+                    <TouchableOpacity style={styles.icon}>
+                        <Icon
+                            name='bookmark-outline'
+                            iconFamily='Ionicons'
+                            color={Colors.primary}
+                            size={16}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.icon}>
+                        <Icon
+                            name='share-outline'
+                            iconFamily='Ionicons'
+                            color={Colors.primary}
+                            size={16}
+                        />
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+            <ScrollView contentContainerStyle={styles.scrollContainer}>
+                {item?.customizationOptions?.map((customization: any, index: number) => {
+                    return (
+                        <View style={styles.subContainer} key={index}>
+                            <CustomText fontFamily='Okra-Medium'>
+                                {customization?.type}
+                            </CustomText>
+                            <CustomText fontFamily='Okra-Medium' varient='h7' color='#888'>
+                                {customization?.required
+                                    ? `Required . Select any 1 option`
+                                    : `Add on your ${customization?.type}`
+                                }
+                            </CustomText>
+                            <DottedLine />
+                            {
+                                customization?.options?.map((option: any, i: number) => {
+                                    return (
+                                        <TouchableOpacity
+                                            key={i}
+                                            style={styles.optionContainer}
+                                            onPress={() => {
+                                                selectOptionHandler(customization?.type, i)
+                                            }}
+                                        >
+                                            <CustomText fontFamily='Okra-Medium' fontSize={11}>
+                                                {option?.name}
+                                            </CustomText>
+                                            <View style={styles.flexRowGap}>
+                                                <CustomText fontSize={11} fontFamily='Okra-Medium'>
+                                                    ₹{option?.price}
+                                                </CustomText>
+                                                <Icon
+                                                    name={
+                                                        data?.selectedOption[customization?.type] === i
+                                                            ? 'radiobox-marked'
+                                                            : 'radiobox-blank'
+                                                    }
+                                                    iconFamily='MaterailCommunityIcons'
+                                                    color={
+                                                        data?.selectedOption[customization.type] === i
+                                                            ? Colors.active
+                                                            : '#888'
+                                                    }
+                                                    size={16}
+                                                />
+                                            </View>
+                                        </TouchableOpacity>
+                                    )
+                                })
+                            }
+                        </View>
+                    )
+                })}
+            </ScrollView>
+
+            <View style={styles.footerContainer}>
+                <View style={styles.selectedContainer}>
+                    <ScalePress onPress={removeCartHandler}>
+                        <Icon
+                            name='minus-thick'
+                            iconFamily='MaterailCommunityIcons'
+                            color={Colors.active}
+                            size={RFValue(13)}
+                        />
+                    </ScalePress>
+                    <AnimatedNumber
+                        includeComma={false}
+                        animationDuration={300}
+                        animateToNumber={data?.quantity}
+                        fontStyle={styles.animatedCount}
+                    />
+                    <ScalePress onPress={addCartHandler}>
+                        <Icon
+                            name='plus-thick'
+                            iconFamily='MaterailCommunityIcons'
+                            size={RFValue(13)}
+                            color={Colors.active}
+                        />
+                    </ScalePress>
+                </View>
+
+                <TouchableOpacity
+                    style={styles.addButtonContainer}
+                    onPress={addItemIntoCart}>
+                    <CustomText color='#fff' fontFamily='Okra-Medium' varient='h5'>
+                        Add item - ₹{data?.price}
+                    </CustomText>
+                </TouchableOpacity>
+                <SafeAreaView />
+            </View>
         </View>
     )
 }
--- src/states/reducers/cartSlice.tsx
+++ src/states/reducers/cartSlice.tsx
@@ -112,10 +112,91 @@ export const cartSlice = createSlice({
                     quantity: number;
                     price: number;
                     customizationOptions: any[]
-                }
+                };
             }>
         ) => {
+            const { resturant, item, costomization } = action.payload;
+            const existingRestaurantCart = state.carts.find(
+                cart => cart.resturant.id === resturant.id
+            );
 
+            if (existingRestaurantCart) {
+
+                const existingItem = existingRestaurantCart?.items?.find(
+                    cartItem => cartItem?.id === item?.id
+                ) as any;
+
+                if (existingItem) {
+
+                    const existingCustomizationIndex =
+                        existingItem?.cusomizations?.findIndex(
+                            (cust: any) =>
+                                JSON.stringify(cust.customizationOptions) ===
+                                JSON.stringify(costomization.customizationOptions)
+                        )
+
+                    if (
+                        existingCustomizationIndex !== undefined &&
+                        existingCustomizationIndex !== -1
+                    ) {
+                        const existingCustomization =
+                            existingItem?.customizations[existingCustomizationIndex];
+                        existingCustomization.quantity += costomization?.quantity;
+                        existingCustomization.cartPrice += costomization?.price;
+                    } else {
+                        const newCustomizationId = uuid();
+                        existingItem?.customizations?.push({
+                            id: newCustomizationId,
+                            ...costomization,
+                            quantity: costomization?.quantity,
+                            cartPrice: costomization?.price,
+                            price: costomization?.price / costomization?.quantity
+                        })
+                    }
+
+                    existingItem.quantity += costomization?.quantity
+                    existingItem.cartPrice = (existingItem?.cartPrice || 0) + costomization?.price
+
+                } else {
+                    const newCustomizationid = `c1`;
+                    existingRestaurantCart.items.push({
+                        ...item,
+                        quantity: costomization?.quantity,
+                        cartPrice: costomization?.price,
+                        customizations: [
+                            {
+                                id: newCustomizationid,
+                                ...costomization,
+                                quantity: costomization?.quantity,
+                                cartPrice: costomization?.price,
+                                price: costomization.price / costomization.quantity
+                            }
+                        ]
+                    })
+                }
+
+            } else {
+                const newCustomizationid = `c1`;
+                state.carts.push({
+                    resturant,
+                    items: [
+                        {
+                            ...item,
+                            quantity: costomization?.quantity,
+                            cartPrice: costomization?.price,
+                            customizations: [
+                                {
+                                    id: newCustomizationid,
+                                    ...costomization,
+                                    quantity: costomization?.quantity,
+                                    cartPrice: costomization?.price,
+                                    price: costomization.price / costomization.quantity
+                                }
+                            ]
+                        }
+                    ]
+                })
+            }
         },
 
         removeCustomizableItem: (
@@ -126,7 +207,47 @@ export const cartSlice = createSlice({
                 customizationId: string;
             }>
         ) => {
+            const { resturant_id, itemId, customizationId } = action.payload;
+            const restaurantCart = state?.carts?.find(
+                cart => cart?.resturant?.id === resturant_id,
+            );
 
+            if (!restaurantCart) return;
+
+            const item = restaurantCart?.items?.find(
+                cartItem => cartItem?.id === itemId,
+            );
+
+            if (!item) return;
+
+            const customizationIndex = item?.customizations?.findIndex(
+                cust => cust?.id === customizationId,
+            ) as number;
+
+            if (customizationIndex !== -1 && item?.customizations) {
+                const customization = item.customizations[customizationIndex];
+                if (customization?.quantity > 1) {
+                    customization.quantity -= 1;
+                    customization.cartPrice -= customization?.price;
+                } else {
+                    item?.customizations?.splice(customizationIndex, 1)
+                }
+
+                item.quantity -= 1;
+                item.cartPrice = (item?.cartPrice || 0) - customization?.price;
+
+                if (item?.quantity === 0 || item?.customizations?.length === 0) {
+                    restaurantCart.items = restaurantCart?.items?.filter(
+                        cartItem => cartItem.id !== itemId,
+                    )
+                }
+
+                if (restaurantCart?.items?.length === 0) {
+                    state.carts = state.carts?.filter(
+                        cart => cart?.resturant?.id !== resturant_id,
+                    )
+                }
+            }
         },
 
         updateCustomizableItem: (
@@ -141,7 +262,57 @@ export const cartSlice = createSlice({
                     customizaionOptions: any[];
                 }
             }>
-        ) => { },
+        ) => {
+            const { resturant_id, itemId, customizationId, newCustomization } = action.payload;
+
+            const restaurantCart = state.carts.find(
+                cart => cart.resturant.id === resturant_id
+            );
+            if (!restaurantCart) return;
+
+            const item = restaurantCart.items.find(
+                cartItem => cartItem.id === itemId
+            );
+            if (!item || !item.customizations) return;
+
+            const matchingCustomizationIndex = item?.customizations?.findIndex(
+                (cust: any) =>
+                    cust?.id !== customizationId && JSON.stringify(cust.customizaionOptions) ===
+                    JSON.stringify(newCustomization.customizaionOptions)
+            )
+
+            const targetCustomizationIndex = item?.customizations?.findIndex(
+                cust => cust.id === customizationId
+            );
+            if (targetCustomizationIndex === -1) return;
+
+            const targetCustomization = item?.customizations[targetCustomizationIndex]
+
+            if (matchingCustomizationIndex !== -1) {
+                const matchingCustomization = item.customizations[matchingCustomizationIndex]
+
+                matchingCustomization.quantity += newCustomization?.quantity;
+                matchingCustomization.cartPrice += newCustomization?.price;
+
+                item?.customizations?.splice(targetCustomizationIndex, 1)
+            } else {
+                targetCustomization.quantity = newCustomization.quantity;
+                targetCustomization.cartPrice = newCustomization.price;
+                targetCustomization.price = newCustomization.price / newCustomization.quantity;
+                targetCustomization.customizaionOptions = newCustomization.customizaionOptions
+            }
+
+
+            item.quantity = item?.customizations?.reduce(
+                (sum, cust) => sum + cust.quantity,
+                0
+            )
+            item.cartPrice = item?.customizations?.reduce(
+                (sum, cust) => sum + cust.cartPrice,
+                0
+            )
+
+        },
 
 
         clearAllCarts: (state) => {
